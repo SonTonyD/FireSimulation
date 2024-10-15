98697cab591d69344a2b727245334bac80e22f6d
SonTonyD <sontony.dinh@gmail.com>
2024-10-16 00:08:21 +0200

    [feature]: migrate fire into Fragment

--- app/src/main/java/com/example/firesimulation/Fire.kt
+++ app/src/main/java/com/example/firesimulation/Fire.kt
@@ -5,6 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ListView
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -21,6 +25,10 @@ class Fire : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
 
+    private lateinit var listView: ListView
+    private lateinit var adapter: ArrayAdapter<String>
+    private val items = mutableListOf<String>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
@@ -29,12 +37,65 @@ class Fire : Fragment() {
         }
     }
 
+    private fun onCalculateButtonClick(initialAmount: Double, savePerMonth: Double, rate: Double, age: Double) {
+        var result: Double
+        var patrimoine: Double = initialAmount
+
+        var resultWithoutRate: Double
+        var patrimoineWithoutRate: Double = initialAmount
+
+        for (i in age.toInt()..age.toInt()+45) {
+            result = (patrimoine + 12*savePerMonth)*rate
+            patrimoine = result
+
+            resultWithoutRate = (patrimoineWithoutRate + 12*savePerMonth)*1
+            patrimoineWithoutRate = resultWithoutRate
+
+
+            addItem("$i ans , ${patrimoine.toInt()}, $patrimoineWithoutRate")
+        }
+    }
+
+    private fun addItem(item: String) {
+        items.add(item)
+        adapter.notifyDataSetChanged()
+    }
+
+    private fun clearListView() {
+        items.clear()
+        adapter.notifyDataSetChanged()
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_fire, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val initialAmountEditText: EditText = view.findViewById<EditText?>(R.id.initialAmount)
+        val savePerMonthEditText: EditText = view.findViewById(R.id.savePerMonth)
+        val rateEditText: EditText = view.findViewById(R.id.rate)
+        val ageEditText: EditText = view.findViewById(R.id.age)
+
+        listView = view.findViewById(R.id.listView)
+        adapter = ArrayAdapter(requireContext(), android.R.layout.simple_list_item_1, items)
+        listView.adapter = adapter
+
+        val calculateButton: Button = view.findViewById(R.id.button)
+        calculateButton.setOnClickListener {
+            clearListView()
+            onCalculateButtonClick(
+                initialAmountEditText.text.toString().toDouble(),
+                savePerMonthEditText.text.toString().toDouble(),
+                rateEditText.text.toString().toDouble(),
+                ageEditText.text.toString().toDouble(),
+            )
+        }
     }
 
     companion object {
