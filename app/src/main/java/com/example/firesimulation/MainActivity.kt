471f0784cc9a55960655ba76926a3337c3934b42
SonTonyD <sontony.dinh@gmail.com>
2024-10-17 12:24:34 +0200

    [feature]: finish the Salary screen

--- app/src/main/java/com/example/firesimulation/MainActivity.kt
+++ app/src/main/java/com/example/firesimulation/MainActivity.kt
@@ -1,14 +1,7 @@
 package com.example.firesimulation
 
 import android.os.Bundle
-import android.widget.ArrayAdapter
-import android.widget.Button
-import android.widget.EditText
-import android.widget.ListView
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.Fragment
 import com.example.firesimulation.databinding.ActivityMainBinding
 
@@ -38,69 +31,4 @@ class MainActivity : AppCompatActivity() {
         fragmentTransaction.replace(R.id.frame_layout, fragment)
         fragmentTransaction.commit()
     }
-
-
-
-//    private lateinit var listView: ListView
-//    private lateinit var adapter: ArrayAdapter<String>
-//    private val items = mutableListOf<String>()
-//
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        enableEdgeToEdge()
-//        setContentView(R.layout.activity_main)
-//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-//            insets
-//        }
-//        val initialAmountEditText: EditText = findViewById<EditText?>(R.id.initialAmount)
-//        val savePerMonthEditText: EditText = findViewById(R.id.savePerMonth)
-//        val rateEditText: EditText = findViewById(R.id.rate)
-//        val ageEditText: EditText = findViewById(R.id.age)
-//
-//        listView = findViewById(R.id.listView)
-//        adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, items)
-//        listView.adapter = adapter
-//
-//        val calculateButton: Button = findViewById(R.id.button)
-//        calculateButton.setOnClickListener {
-//            clearListView()
-//            onCalculateButtonClick(
-//                initialAmountEditText.text.toString().toDouble(),
-//                savePerMonthEditText.text.toString().toDouble(),
-//                rateEditText.text.toString().toDouble(),
-//                ageEditText.text.toString().toDouble(),
-//            )
-//        }
-//    }
-//
-//    private fun onCalculateButtonClick(initialAmount: Double, savePerMonth: Double, rate: Double, age: Double) {
-//        var result: Double
-//        var patrimoine: Double = initialAmount
-//
-//        var resultWithoutRate: Double
-//        var patrimoineWithoutRate: Double = initialAmount
-//
-//        for (i in age.toInt()..age.toInt()+45) {
-//            result = (patrimoine + 12*savePerMonth)*rate
-//            patrimoine = result
-//
-//            resultWithoutRate = (patrimoineWithoutRate + 12*savePerMonth)*1
-//            patrimoineWithoutRate = resultWithoutRate
-//
-//
-//            addItem("$i ans , ${patrimoine.toInt()}, $patrimoineWithoutRate")
-//        }
-//    }
-//
-//    private fun addItem(item: String) {
-//        items.add(item)
-//        adapter.notifyDataSetChanged()
-//    }
-//
-//    private fun clearListView() {
-//        items.clear()
-//        adapter.notifyDataSetChanged()
-//    }
 }
--- app/src/main/java/com/example/firesimulation/Salary.kt
+++ app/src/main/java/com/example/firesimulation/Salary.kt
@@ -1,10 +1,16 @@
 package com.example.firesimulation
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Switch
+import com.google.android.material.textfield.TextInputLayout
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -35,6 +41,130 @@ class Salary : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_salary, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val clearInput: Button = view.findViewById(R.id.clearInputButton)
+
+        // Récupération des EditText à partir des TextInputLayout
+        val mensuelBrutLayout: TextInputLayout? = view.findViewById(R.id.mensuelBrut)
+        val mensuelNetLayout: TextInputLayout? = view.findViewById(R.id.mensuelNet)
+        val annuelBrutLayout: TextInputLayout? = view.findViewById(R.id.annuelBrut)
+        val annuelNetLayout: TextInputLayout? = view.findViewById(R.id.annuelNet)
+
+        val mensuelBrut = mensuelBrutLayout?.editText
+        val mensuelNet = mensuelNetLayout?.editText
+        val annuelBrut = annuelBrutLayout?.editText
+        val annuelNet = annuelNetLayout?.editText
+
+        // Variable pour bloquer la boucle de TextWatcher
+        var isUpdating = false
+
+        // Fonction pour calculer et remplir les champs
+        fun updateFields(source: EditText) {
+            if (isUpdating) return
+
+            var taxeValue = 1-0.22
+
+            if (view.findViewById<Switch>(R.id.cadreSwitch).isChecked) {
+                taxeValue = (1-0.25)
+            }
+
+            isUpdating = true  // Bloquer les autres updates
+
+            val sourceValue = source.text.toString().toDoubleOrNull() ?: return
+
+            when (source) {
+                mensuelBrut -> {
+                    val netMensuel = sourceValue * taxeValue
+                    mensuelNet?.setText("%.2f".format(netMensuel))
+
+                    val brutAnnuel = sourceValue * 12
+                    annuelBrut?.setText("%.2f".format(brutAnnuel))
+
+                    val netAnnuel = netMensuel * 12
+                    annuelNet?.setText("%.2f".format(netAnnuel))
+                }
+
+                mensuelNet -> {
+                    val brutMensuel = sourceValue / taxeValue
+                    mensuelBrut?.setText("%.2f".format(brutMensuel))
+
+                    val brutAnnuel = brutMensuel * 12
+                    annuelBrut?.setText("%.2f".format(brutAnnuel))
+
+                    val netAnnuel = sourceValue * 12
+                    annuelNet?.setText("%.2f".format(netAnnuel))
+                }
+
+                annuelBrut -> {
+                    val mensuelBrutValue = sourceValue / 12
+                    mensuelBrut?.setText("%.2f".format(mensuelBrutValue))
+
+                    val netMensuel = mensuelBrutValue * taxeValue
+                    mensuelNet?.setText("%.2f".format(netMensuel))
+
+                    val netAnnuel = sourceValue * taxeValue
+                    annuelNet?.setText("%.2f".format(netAnnuel))
+                }
+
+                annuelNet -> {
+                    val netMensuel = sourceValue / 12
+                    mensuelNet?.setText("%.2f".format(netMensuel))
+
+                    val brutMensuel = netMensuel / taxeValue
+                    mensuelBrut?.setText("%.2f".format(brutMensuel))
+
+                    val brutAnnuel = brutMensuel * 12
+                    annuelBrut?.setText("%.2f".format(brutAnnuel))
+                }
+            }
+
+            isUpdating = false  // Permettre à nouveau les updates
+        }
+
+        // Ajoute des TextWatcher pour chaque champ
+        mensuelBrut?.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+            override fun afterTextChanged(s: Editable?) {
+                updateFields(mensuelBrut)
+            }
+        })
+
+        mensuelNet?.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+            override fun afterTextChanged(s: Editable?) {
+                updateFields(mensuelNet)
+            }
+        })
+
+        annuelBrut?.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+            override fun afterTextChanged(s: Editable?) {
+                updateFields(annuelBrut)
+            }
+        })
+
+        annuelNet?.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+            override fun afterTextChanged(s: Editable?) {
+                updateFields(annuelNet)
+            }
+        })
+
+        clearInput.setOnClickListener({
+            mensuelBrut?.text?.clear()
+            mensuelNet?.text?.clear()
+            annuelBrut?.text?.clear()
+            annuelNet?.text?.clear()
+            isUpdating = false
+        })
     }
 
     companion object {
